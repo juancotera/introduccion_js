313a66a9d61562c046d78a62d42774bd93da22df
juancotera <juancoteracastro@gmail.com>
2022-08-01 19:36:29 -0500

    intro

---
+++ main.js
@@ -0,0 +1,41 @@
+//VARIABLES:
+//Var
+var nombre = 'Juan'
+console.log(nombre)
+//let
+let apellido = 'Cotera'
+console.log(apellido)
+//const
+const cursos = 13
+console.log(cursos)
+
+//OPERADORES
+10 + 5 // suma
+10 - 5 // resta
+5 * 5 // multiplicación
+10 / 2 // divición
+5 % 2 // residuo
+Math.pow(5, 2) // exponente
+Math.sqrt(9) // Raiz cuadrada
+
+//FALSY VALUES
+Bolean('') // false
+Bolean("") // false
+Bolean(``) // false
+Bolean(0) // false
+Bolean(-0) // false
+Bolean(0n) // false
+Bolean(NaN) // false
+Bolean(null) // false
+Bolean(undefined) // false
+
+//TRUTHLY VALUES
+Bolean('Juan') // true
+Bolean(-1) // true
+Bolean(1.1) // true
+Bolean(1n) // true
+Bolean(Infinity) // true
+Bolean(-Infinity) // true
+Bolean({}) // true
+Bolean([]) // true
+Bolean(new Date()) // true
