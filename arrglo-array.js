e0d3a8b1e98760526aedc5a5c8eef64593dcbe3c
juancotera <juancoteracastro@gmail.com>
2022-08-02 20:27:08 -0500

    array multidimensional

--- arrglo-array.js
+++ arrglo-array.js
@@ -1,3 +1,9 @@
 // ARREGLO/ARRAY
+// Se añaden en [] separando con ,
 const myArray = ["perro", "gato", "canario", 2]
 console.log(myArray);
+
+// ARRAY ANIDADOS O MULTIDEMENCIONAL
+// Podemos incluir arreglos dentro de otros, usando []
+const myArrayM = [["Primero", "A", 23], ["Primero", "B", 25]];
+console.log(myArrayM);
