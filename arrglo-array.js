75d63847022e7e04432f71acacc08469bbc9e909
juancotera <juancoteracastro@gmail.com>
2022-08-02 21:15:38 -0500

    acceder a arreglos multidimencionales

--- arrglo-array.js
+++ arrglo-array.js
@@ -7,3 +7,27 @@ console.log(myArray);
 // Podemos incluir arreglos dentro de otros, usando []
 const myArrayM = [["Primero", "A", 23], ["Primero", "B", 25]];
 console.log(myArrayM);
+
+//ACCEDER DATOS DENTRO DE UN ARRAY
+const calificaciones = [15, 17, 16, 20];
+const calificacionMayor = calificaciones[3];
+console.log(calificacionMayor);
+
+// CAMBIAR LOS VALORES DE UNA ARRAY
+const califBim = [14, 16, 18, 20];
+califBim[3] = 14;
+
+console.log(califBim);
+
+// ACCEDER A VALORES MULTIDIMENCIONALES
+const notas = [
+  [15, 14, 16],
+  [16, 15, 18],
+  [[12, 13, 20], 19, 18],
+  [14, 18, 19]
+];
+// Debemos ubicar el arreglo usando [] sin separarlos
+//const notaMayor = notas[2]; nos dará [[12, 13, 20], 19, 18]
+//const notaMayor = notas[2][0]; nos dará [12, 13, 20]
+const notaMayor = notas[2][0][2]; // 20
+console.log(notaMayor);
