41a023f5dbdf926a26e1e943005e79d654d669a8
juancotera <juancoteracastro@gmail.com>
2023-08-29 11:54:04 -0500

    nuevo ejercicio

---
+++ DOM/01-colorsGenerator/app.js
@@ -0,0 +1,12 @@
+const colors = ["red", "green", "blue", "white", "purple", "#414141", "brown", "pink"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function() {
+  //const randomNumber = Math.floor(Math.random() * colors.length);
+  //console.log(randomNumber);
+
+  document.body.style.backgroundColor = colors[randomNumber];
+  //color.textContent = colors[randomNumber];
+});
+
